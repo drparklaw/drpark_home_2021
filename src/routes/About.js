beae0011ecb7e6b1e58008a8d33865f4b8940d80
drparklaw <drparklaw@gmail.com>
2021-01-19 12:59:01 +0900

    논문업데이트

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,7 +6,7 @@ import Career from '../components/Career';
 function About() {
   return (
     <div>
-      <h3>주요 경력</h3>
+      <h3>주요 이력</h3>
       <Career />
       <h3>법학 논문</h3>
       <Papers />
