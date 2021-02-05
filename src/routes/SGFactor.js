ccd34006663f547ab3ef98a9d9dd8bcecccc6a0e
drparklaw <drparklaw@gmail.com>
2021-02-05 15:40:55 +0900

    범죄통계

--- src/routes/SGFactor.js
+++ src/routes/SGFactor.js
@@ -7,13 +7,20 @@ function SGFactor() {
   return (
 
     <div>
-      <Title text="범죄 통계 (소스: 국가통계포털)"/>
+      <Title text="범죄 통계"/>
 
       <h3><i class="fas fa-gavel"></i> 시간당 1건 이상 발생하는 범죄</h3>
       <CrimeList />
       <br />
       <h3><i class="fas fa-gavel"></i> 단독범죄가 전체범죄의 90% 이상</h3>
       <UnilateralCrime />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
 
   );
