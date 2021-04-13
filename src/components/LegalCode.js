4f140a3b287a30647f1076cbc0e7bf9288d1b7ce
drparklaw <drparklaw@gmail.com>
2021-04-13 10:46:48 +0900

    lawtlin start

--- src/components/LegalCode.js
+++ src/components/LegalCode.js
@@ -9,81 +9,81 @@ const kotlin_codes = [
         name: `인(Person)`,
         note: `인(人)은 권리를 가지고 의무를 부담할 수 있는 자격, 즉 권리능력을 가지는 주체를 말합니다`,
         code: `
-    open class Person(_name: String){
-        // 이름
-        var name: String = _name
+open class Person(_name: String){
+    // 이름
+    var name: String = _name
         
-        var birth: Date? = null
-        var death: Date? = null
+    var birth: Date? = null
+    var death: Date? = null
         
-        var age: Int? = null
+    var age: Int? = null
                 
-        constructor(_name: String, _birth: Date): this(_name) {
-            birth = _birth
-        }
-        
-        constructor(_name: String, _birth: Date, _death: Date): this(_name, _birth) {
-            death = _death
-        }
-        
-        override fun toString(): String {
-            return name
-        }
+    constructor(_name: String, _birth: Date): this(_name) {
+        birth = _birth
     }
-    `
+        
+    constructor(_name: String, _birth: Date, _death: Date): this(_name, _birth) {
+        death = _death
+    }
+        
+    override fun toString(): String {
+        return name
+    }
+}
+`
     },
     {
         id: 2,
         name: `자연인(Natural Person)`,
         note: `사람은 생존한 동안 권리와 의무의 주체가 됩니다`,
         code: `
-    class NaturalPerson(_name: String): Person(_name) {
+class NaturalPerson(_name: String): Person(_name) {
 
-        // 태아 여부
-        var fetus: Boolean = false
+    // 태아 여부
+    var fetus: Boolean = false
         
-        // 성별
-        var sex: Sex = Sex.MALE
+    // 성별
+    var sex: Sex = Sex.MALE
         
-        // 미성년자 여부
-        var minor: Boolean = false
+    // 미성년자 여부
+    var minor: Boolean = false
         
-        // 배우자
-        var spouse: NaturalPerson? = null
+    // 배우자
+    var spouse: NaturalPerson? = null
         
-        // 부모
-        var father: NaturalPerson? = null
-        var mother: NaturalPerson? = null
+    // 부모
+    var father: NaturalPerson? = null
+    var mother: NaturalPerson? = null
         
-        // 직계비속
-        var linealDescendant = mutableListOf<NaturalPerson>()
+    // 직계비속
+    var linealDescendant = mutableListOf<NaturalPerson>()
         
-    }
+}
         
-    enum class Sex {
-        MALE, FEMALE
-    }
-    `
+enum class Sex {
+    MALE, FEMALE
+}
+`
     },
     {
         id: 3,
         name: `법인(Legal Person)`,
         note: `법인은 설립등기를 함으로써 성립하고 성질상·법령상·정관상으로 권리능력이 제한됩니다`,
         code: `
-    class LegalPerson(_name: String): Person(_name) {
+class LegalPerson(_name: String): Person(_name) {
 
-        // 대표
-        var representative: NaturalPerson? = null
+    // 대표
+    var representative: NaturalPerson? = null
         
-        // 이사
-        var director = mutableListOf<NaturalPerson>()
+    // 이사
+    var director = mutableListOf<NaturalPerson>()
         
-        constructor(_name: String, _representative: NaturalPerson): this(_name) {
-            representative = _representative
-        }
-        
+    constructor(_name: String, _representative: NaturalPerson): this(_name) {
+        representative = _representative
     }
-    `
+        
+}
+`
     }
 
 ]
