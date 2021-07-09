ce324f16da1d2f55aed56a2d1b65218ec31ddcf9
unknown <drparklaw@gmail.com>
2021-07-09 14:07:35 +0900

    del

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import Image from '../components/Image';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">In law school I'm doing research and development on AI Lawyer of South Korea.</p>
+            <p class="IntroText">In law school I'm doing research and development on AI Lawyer of Korea.</p>
             <Image url="drparkPicture.jpg" caption="Picture in 2014" />
         </div>
     );
