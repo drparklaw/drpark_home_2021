ca1ff068a707cbc7a8e516f2f2a3bb4a0f7cf8ff
unknown <drparklaw@gmail.com>
2021-07-09 12:30:35 +0900

    del

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './css/styles.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
-import SGFactor from './routes/SGFactor';
 import Lawtlin from './routes/Lawtlin';
 import Navigation from './components/Navigation';
 
@@ -12,7 +11,6 @@ function App() {
     <HashRouter>
       <Navigation />
       <Route path="/" exact={true} component={About}/>
-      <Route path="/sgfactor" component={SGFactor} />
       <Route path="/lawtlin" component={Lawtlin} />
     </HashRouter>
   );
