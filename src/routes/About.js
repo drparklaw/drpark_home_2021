78dfd78967d2a72f2fa5b1253097dc049eed15a3
unknown <drparklaw@gmail.com>
2021-07-09 12:16:00 +0900

    del

--- src/routes/About.js
+++ src/routes/About.js
@@ -12,7 +12,9 @@ function About() {
     <div>
       <Title text="안녕하세요 박봉철입니다"/>
       <Intro />
+
       <NameCard />
+      <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fdrpark.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
       <h3>주요 이력</h3>
       <Career />
       <h3>법학 논문</h3>
--- src/routes/SGFactor.js
+++ src/routes/SGFactor.js
@@ -9,7 +9,7 @@ function SGFactor() {
     <div class="crimeTable">
       <Title text="범죄 통계"/>
 
-      <h3><i class="fas fa-gavel"></i> 시간당 1건 이상 발생하는 범죄</h3>
+      <h3><i class="fas fa-gavel"></i> 두둥시간당 1건 이상 발생하는 범죄</h3>
       <CrimeList />
       <br />
       <h3><i class="fas fa-gavel"></i> 단독범죄가 전체범죄의 90% 이상</h3>
