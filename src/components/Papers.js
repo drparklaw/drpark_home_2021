0df4d5d02f602175ab4baee1a139d8e834fa2d99
unknown <drparklaw@gmail.com>
2021-07-12 16:31:18 +0900

    del

--- src/components/Papers.js
+++ src/components/Papers.js
@@ -54,6 +54,15 @@ const drpark_papers = [
         institute: '동아법학전문대학원',
         year: '2021.2',
         page: 'pp.45-65'
+    },
+    {
+        id: 7,
+        name: '박봉철',
+        title: '"인공지능 변호사를 위한 법리의 구조화와 그 표현"',
+        journal: '국제교류와 융합교육 제1권 제1호',
+        institute: '한국국제교육교류학회',
+        year: '2021.7',
+        page: 'pp.61-80'
     }
 ]
 
