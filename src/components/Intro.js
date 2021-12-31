28c1ec78c7fb5c7c322ee0722c3384bb1c252594
unknown <drparklaw@gmail.com>
2021-12-31 16:30:18 +0900

    new intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import Image from '../components/Image';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">I am developing an AI lawyer of Korea using predicate logic, fuzzy theory, and other math tools for legal data in Blockchain.</p>
+            <p class="IntroText">I am developing an AI lawyer of Korea using solid mathematical theories for legal data in Blockchain.</p>
             <Image url="drparkPicture.jpg" caption="Picture in 2014" />
         </div>
     );
