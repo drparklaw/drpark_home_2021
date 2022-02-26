827fde89aef5206033dfad36156b898a9ea0e670
unknown <drparklaw@gmail.com>
2022-02-26 11:47:54 +0900

    menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ function Navigation() {
 
     return (
         <div className="nav">
-            <Link to="/">인사말씀</Link>
+            {/* <Link to="/">인사말씀</Link> */}
             {/* <Link to="/lawtlin">전산법학</Link> */}
         </div>
     );
