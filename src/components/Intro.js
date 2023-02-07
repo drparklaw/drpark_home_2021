d41edc93c4a733b9b527bc0c0d00e698c069382c
unknown <drparklaw@gmail.com>
2023-02-07 10:07:55 +0900

    career

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import Image from '../components/Image';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">I am developing an AI lawyer of Korea using solid math theories for legal data in Blockchain.</p>
+            <p class="IntroText">I am developing <span class="emphasis">an AI lawyer of Korea</span> using solid math theories for legal data in Blockchain.</p>
             <Image url="drparkPicture.jpg" caption="Picture in 2014" />
         </div>
     );
