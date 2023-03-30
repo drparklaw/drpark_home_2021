3b6afbdd4a3b34e398f401c961b035041cc664a1
unknown <drparklaw@gmail.com>
2023-03-30 22:35:30 +0900

    intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,7 @@ function Intro() {
     return (
         <div class="IntroWrapper">
             <p class="IntroText">반갑습니다. 저는 동아대 로스쿨에서 민사실무 등을 강의합니다. 평소 법률데이터 구축이나 인공지능 변호사에 관하여 고민하고 있습니다. 궁금하신 사항은 편하게 연락주시기 바랍니다.</p>
-            <Image url="myOffice.png"/>
+            <Image url="myOffice.png" caption="What truly is logic? ..." />
         </div>
     );
 }
