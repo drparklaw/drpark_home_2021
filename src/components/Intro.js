89e2bbc1d89ffe74824a5893566b5b98e25f7c0d
unknown <drparklaw@gmail.com>
2023-03-30 21:11:39 +0900

    intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import Image from '../components/Image';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">환영합니다. 동아로스쿨의 박봉철 입니다. 저는 법학과 공학을 융합해서 새로운 디지털 콘텐츠를 만들고 있습니다. 함께 하실 분은 언제든 연락주세요.</p>
+            <p class="IntroText">환영합니다. 저는 동아대 로스쿨에서 민사실무 등을 강의하고 있습니다. 평소 한국의 인공지능 변호사 개발에 관심이 많은데요, 함께 하실 분은 언제든지 연락주세요.</p>
             <Image url="drparkPicture.jpg" caption="Picture in 2014" />
         </div>
     );
