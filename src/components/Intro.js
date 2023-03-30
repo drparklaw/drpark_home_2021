a756dc05375b039be50bb7328dced826ddd6e919
unknown <drparklaw@gmail.com>
2023-03-30 20:50:44 +0900

    intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import Image from '../components/Image';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">환영합니다. 저는 공학과 법학을 융합하여 세상에 없던 새로운 콘텐츠를 만들고 있습니다. 함께 하실 분은 지금 연락주세요.</p>
+            <p class="IntroText">환영합니다. 동아로스쿨의 박봉철 입니다. 저는 법학과 공학을 융합해서 새로운 디지털 콘텐츠를 만들고 있습니다. 함께 하실 분은 언제든 연락주세요.</p>
             <Image url="drparkPicture.jpg" caption="Picture in 2014" />
         </div>
     );
