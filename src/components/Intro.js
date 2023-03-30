3631fdc65e97a40fca047377b8e35f22f56cf79a
unknown <drparklaw@gmail.com>
2023-03-30 20:33:53 +0900

    intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import Image from '../components/Image';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">환영합니다. 저는 공학과 법학을 융합하여 새로운 디지털 콘텐츠를 만들고 있습니다. 궁금하신 점이 있다면 편하게 연락주시기 바랍니다.</p>
+            <p class="IntroText">환영합니다. 저는 공학과 법학을 융합하여 세상에 없던 새로운 콘텐츠를 만들고 있습니다. 함께 하실 분은 지금 연락주세요.</p>
             <Image url="drparkPicture.jpg" caption="Picture in 2014" />
         </div>
     );
