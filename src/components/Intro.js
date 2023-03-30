5e60b7471666bc9d64eaa19f225e804905258285
unknown <drparklaw@gmail.com>
2023-03-30 21:33:26 +0900

    intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,8 +5,8 @@ import Image from '../components/Image';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">환영합니다. 저는 동아대 로스쿨에서 민사실무 등을 강의하고 있습니다. 평소 한국의 인공지능 변호사 개발에 관심이 많은데요, 함께 하실 분은 언제든지 연락주세요.</p>
-            <Image url="drparkPicture.jpg" caption="Picture in 2014" />
+            <p class="IntroText">반갑습니다. 저는 동아대 로스쿨에서 민사실무 등을 강의하고 있습니다. 평소 한국의 인공지능 변호사 개발에 관심이 많은데요, 함께 하실 분은 언제든지 연락주세요.</p>
+            <Image url="myOffice.png" caption="" />
         </div>
     );
 }
