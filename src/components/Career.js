aed62e4d209364b1baf16d9b0f31f519ab8d8909
drparklaw <drparklaw@gmail.com>
2023-05-24 17:38:00 +0900

    first

--- src/components/Career.js
+++ src/components/Career.js
@@ -5,15 +5,15 @@ function Career() {
 
     return (
         /*<ol class="MethodWrapper">*/
-        <ol>
+        <ul>
             <li>경남과고/KAIST96학번/동아로스쿨3기</li>
             <li>KAIST 전산학박사(2003~2007)</li>
             <li>삼성종합기술원 전문연구원(2008~2010)</li>
             <li>변호사 박봉철 법률사무소(2014~2020)</li>
             <li>現 동아법학전문대학원 조교수</li>
-            <li>現 부산대학교병원 데이터심의위원회 위원</li>
+            <li>現 부산대병원 데이터심의위원회 위원</li>
             <li>現 대한상사중재원 중재인</li>
-        </ol>); 
+        </ul>); 
 }
 
 export default Career;
