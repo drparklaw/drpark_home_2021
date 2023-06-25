0c1cdaf54e083b44e6f9a6586905442af818940e
drparklaw <drparklaw@gmail.com>
2023-06-25 12:25:21 +0900

    case adding

--- src/routes/SGFactor.js
+++ src/routes/SGFactor.js
@@ -3,6 +3,7 @@ import Title from '../components/Title';
 import CrimeList from '../components/CrimeList';
 import AccompliceCrime from '../components/AccompliceCrime';
 
+
 function SGFactor() {
     return (
   
@@ -16,6 +17,21 @@ function SGFactor() {
         
         <h3><i class="fas fa-gavel"></i> 공범 비율</h3>
         <AccompliceCrime />
+{/* 
+        <h3><i class="fas fa-gavel"></i> 양형데모(사기죄)</h3>
+
+        <div>
+          <p>1. 범죄를 선택하세요</p>
+          <select id="pickCrime" onChange="selectCrime()">
+              <option value="">선택하세요</option>
+              <option value="사기">사기(347조)</option>
+              <option value="컴퓨터등사용사기">컴퓨터등사용사기(347조의2)</option>
+              <option value="준사기">준사기(348조)</option>
+              <option value="상습사기">상습사기(347조, 347조의2, 348조에 한함)</option>
+              <option value="특경법사기">특경법사기(특경법 3조1항)</option>
+          </select>
+        </div> */}
+
         <br />
         <br /> 
         <br />
