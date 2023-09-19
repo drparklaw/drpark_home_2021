8be037fa687ec3c210b7d57a1bbfda7640b36049
drparklaw <drparklaw@gmail.com>
2023-09-19 10:44:25 +0900

    career adding

--- src/components/Career.js
+++ src/components/Career.js
@@ -6,7 +6,7 @@ function Career() {
     return (
         /*<ol class="MethodWrapper">*/
         <ul>
-            <li>경남과고/KAIST96학번/동아로스쿨3기</li>
+            <li>경남과학고/KAIST96학번/동아로스쿨3기</li>
             <li>KAIST 전산학박사(2003~2007)</li>
             <li>삼성종합기술원 전문연구원(2008~2010)</li>
             <li>변호사 박봉철 법률사무소(2014~2020)</li>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import Image from '../components/Image';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">환영합니다. 저는 동아로스쿨에서 <span class="emphasis">민사실무·민사소송법</span> 등을 강의하면서 법률데이터와 AI변호사를 연구하고 있습니다. 많은 관심 부탁드립니다. 오늘도 행복하세요.</p>
+            <p class="IntroText">환영합니다. 저는 동아대 로스쿨에서 <span class="emphasis">민사실무·민사소송법</span> 등을 강의하면서 법률데이터와 AI변호사를 연구하고 있습니다. 많은 관심 부탁드립니다. 오늘도 행복하세요.</p>
             <Image url="myOffice.png" caption="What truly is logic? ..." />
         </div>
     );
