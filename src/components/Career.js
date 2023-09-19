f3264b7e9220cc9193c7327890c9c5c8123a6359
drparklaw <drparklaw@gmail.com>
2023-09-19 11:13:55 +0900

    career adding

--- src/components/Career.js
+++ src/components/Career.js
@@ -6,8 +6,8 @@ function Career() {
     return (
         /*<ol class="MethodWrapper">*/
         <ul>
-            <li>경남과학고/KAIST96학번/동아로스쿨3기</li>
-            <li>KAIST 전산학박사(2003~2007)</li>
+            <li>경남과학고/KAIST96/동아로스쿨3기</li>
+            <li>KAIST 전산학 박사(2003~2007)</li>
             <li>삼성종합기술원 전문연구원(2008~2010)</li>
             <li>변호사 박봉철 법률사무소(2014~2020)</li>
             <li>現 동아법학전문대학원 조교수</li>
