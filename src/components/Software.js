f93c7eae3b203cf36a79346935e9d553a26dc25e
drparklaw <drparklaw@gmail.com>
2023-12-02 08:40:01 +0900

    sw

--- src/components/Software.js
+++ src/components/Software.js
@@ -5,7 +5,7 @@ const drpark_software = [
         id: 1,
         name: 'http://dongalaw.org',
         title: '동아로스쿨 CBT 시스템',
-        publisher: '웹용/맥가능/엣지권장',
+        publisher: '노트북용/Mac가능/Edge권장',
         year: '2023.11.'
     }
 ]
