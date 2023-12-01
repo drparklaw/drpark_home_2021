76918e040ed0a834a2a19e597e6150aef93539b6
drparklaw <drparklaw@gmail.com>
2023-12-02 08:34:51 +0900

    sw

--- src/components/Software.js
+++ src/components/Software.js
@@ -4,8 +4,8 @@ const drpark_software = [
     {
         id: 1,
         name: 'http://dongalaw.org',
-        title: '동아로스쿨 내신 과목을 위한 CBT 시스템',
-        publisher: '엣지(Edge)에 최적화',
+        title: '동아로스쿨 CBT 시스템',
+        publisher: '웹용/맥가능/엣지권장',
         year: '2023.11.'
     }
 ]
