8e5dd163b46e61480a7857fcec2ca42df166dd80
drparklaw <drparklaw@gmail.com>
2024-05-23 06:29:50 +0900

    case

--- src/components/Software.js
+++ src/components/Software.js
@@ -7,6 +7,13 @@ const drpark_software = [
         title: '"동아로스쿨 내신 CBT 시스템"',
         publisher: '노트북용(Mac호환)',
         year: '2023.11'
+    },
+    {
+        id: 2,
+        name: '법무부',
+        title: '"제13회 변호사시험 시험위원"',
+        publisher: '(채점위원)',
+        year: '2024.2'
     }
 ]
 
--- src/routes/About.js
+++ src/routes/About.js
@@ -20,7 +20,7 @@ function About() {
       <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fdrpark.kr&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
       <h3>주요 이력</h3>
       <Career />
-      <h3>법학 SW</h3>
+      <h3>법학 수험</h3>
       <Softwares />
       <h3>법학 저서</h3>
       <Books />
