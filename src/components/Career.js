10bfe653dc8c57bd39314bcd3d15ab4fb9dc6e1a
drparklaw <drparklaw@gmail.com>
2024-08-31 09:55:57 +0900

    revise

--- src/components/Career.js
+++ src/components/Career.js
@@ -6,7 +6,7 @@ function Career() {
     return (
         /*<ol class="MethodWrapper">*/
         <ul>
-            <li>KAIST B.S. (EE & CS), M.S. (EE), and Ph.D. (CS)</li>
+            <li>KAIST B.S.(EE & CS), M.S.(EE), and Ph.D.(CS)</li>
             <li>Senior Researcher, Samsung Advanced Institute of Technology</li>
             <li>Managing Attorney, Dr. Park Law Office</li>
             <li>Associate Professor, Dong-A University Law School (Current)</li>
