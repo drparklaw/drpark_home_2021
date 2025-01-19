c1053a47208f63af8e49976296c45b13c77371c4
drparklaw <drparklaw@gmail.com>
2025-01-19 18:58:25 +0900

    caseRemoved

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,8 +6,8 @@ function Navigation() {
     return (
         <div className="nav">
             <Link to="/">Home</Link>
-            <Link to="/new_cases">Latest Cases</Link>
-            {/*<Link to="/sgfactor/">범죄통계</Link>*/}
+            {/*<Link to="/new_cases">Latest Cases</Link>
+            <Link to="/sgfactor/">범죄통계</Link>*/}
         </div>
     );
 }
