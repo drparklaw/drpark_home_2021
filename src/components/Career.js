f45a49ef9bb9cd12bb46ee3ddcf5a8f2865a6251
drparklaw <drparklaw@gmail.com>
2025-01-22 16:53:22 +0900

    namecard

--- src/components/Career.js
+++ src/components/Career.js
@@ -6,7 +6,7 @@ function Career() {
     return (
         /*<ol class="MethodWrapper">*/
         <ul>
-            <li>KAIST B.S.(EE&CS), M.S.(EE), and Ph.D.(CS)</li>
+            <li>KAIST BS(EE&CS), MS(EE), and PhD(CS)</li>
             <li>Senior Researcher, Samsung Advanced Institute of Technology</li>
             <li>Managing Attorney, Dr. Park Law Office</li>
             <li>Associate Professor, Dong-A University Law School (Present)</li>
