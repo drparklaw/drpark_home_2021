b8f344ce5575e989a74dae020ad424fb7e2ec26b
drparklaw <drparklaw@gmail.com>
2025-04-02 18:41:41 +0900

    industry

--- src/components/Career.js
+++ src/components/Career.js
@@ -7,9 +7,9 @@ function Career() {
         /*<ol class="MethodWrapper">*/
         <ul>
             <li>KAIST BS(EE&CS), MS(EE), and PhD(CS)</li>
-            <li>Senior Researcher, Samsung Advanced Institute of Technology</li>
+            <li>Senior Researcher, Samsung</li>
             <li>Managing Attorney, Dr. Park Law Office</li>
-            <li>Associate Professor, Dong-A University Law School (Present)</li>
+            <li>Associate Professor, Dong-A Univ. Law School (Present)</li>
             <li>Arbitrator, Korean Commercial Arbitration Board (Present)</li>
         </ul>); 
 }
