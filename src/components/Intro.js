2102902110faddd1b14165d0b904d5efb9cf8af4
박봉철 <drpark@Signavy-MacBook-Pro.local>
2025-08-24 18:43:28 +0900

    picture

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,7 @@ function Intro() {
     return (
         <div class="IntroWrapper">
             <p class="IntroText">Greetings, I am a professor at Dong-A Univ. Law School, specializing in Korean civil law. My research focuses on legal data and computational law. Wishing you a wonderful day!</p>
-            <Image url="drpark.jpg" caption="What truly is logic? ..." />
+            <Image url="drpark_png.png" caption="What truly is logic? ..." />
         </div>
     );
 }
