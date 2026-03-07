38139a219f0f581173f8ae9a4bb73c6064633a51
박봉철 <drpark@Signavy-MacBook-Pro.local>
2026-03-07 14:22:56 +0900

    intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,17 +1,15 @@
-import React from "react";
-import ModelViewer from "../components/ModelViewer";
+import React from 'react';
+import Image from '../components/Image';
+
 
 function Intro() {
-  return (
-    <div className="IntroWrapper">
-
-      <p className="IntroText">Welcome! I am a professor at Dong-A University Law School.
+    return (
+        <div class="IntroWrapper">
+            <p class="IntroText">Welcome! I am a professor at Dong-A University Law School.
 My teaching focuses on Korean Civil Law, and my research interests include legal data and computational law.</p>
-
-      <ModelViewer />
-
-    </div>
-  );
+            <Image url="drpark_png.png" caption="What truly is logic? ..." />
+        </div>
+    );
 }
 
 export default Intro;
