d498bf691bd5fbe2546bac27cb1efa52b756ae93
drparklaw <drparklaw@gmail.com>
2026-03-17 16:42:21 +0900

    paper

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Image from '../components/Image';
 
-
 function Intro() {
     return (
-        <div class="IntroWrapper">
-            <p class="IntroText">Welcome! I am a professor at Dong-A University Law School.
-My teaching focuses on Korean Civil Law, and my research interests include legal data and computational law.</p>
+        <div className="IntroWrapper">
+            <p className="IntroText">Welcome! I am a professor at Dong-A University Law School. My teaching focuses on Korean Civil Law, and my research interests include legal data and computational law.</p>
+
             <Image url="drpark_png.png" caption="What truly is logic? ..." />
+
         </div>
     );
 }
@@ -15,42 +15,6 @@ My teaching focuses on Korean Civil Law, and my research interests include legal
 export default Intro;
 
 {/*
-import React from 'react';
-import Image from '../components/Image';
-
-
-function Intro() {
-    return (
-        <div class="IntroWrapper">
-            <p class="IntroText">Welcome! I’m a professor at Dong-A University Law School. While my teaching expertise lies in Korean Civil Law, I am deeply engaged in research regarding legal data and computational law. Have a wonderful day!</p>
-            <Image url="drpark_png.png" caption="What truly is logic? ..." />
-        </div>
-    );
-}
-
-export default Intro;
-
-
-
- import React from 'react';
-import ModelViewer from '../components/ModelViewer';
-
-function Intro() {
-    return (
-        <div className="IntroWrapper">
-            <p className="IntroText">
-            Welcome! I’m a professor at Dong-A University Law School.
-            While my teaching expertise lies in Korean Civil Law, I am deeply
-            engaged in research regarding legal data and computational law.
-            Have a wonderful day!
-            </p>
-
-            <ModelViewer />
-        </div>
-    );
-}
-
-export default Intro;
  여기에 주석을 작성합니다. 이 내용은 화면에 보이지 않습니다. */}
 
 
---
+++ src/components/Lawyer3D.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+
+export default function Lawyer3D() {
+  return (
+    <Canvas style={{ height: "400px", background: "#eeeeee" }}>
+    </Canvas>
+  );
+}
